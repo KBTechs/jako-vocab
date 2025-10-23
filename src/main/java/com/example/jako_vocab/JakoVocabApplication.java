f25f19549b1d1e34ed0eda3ff31a934d964ba276
kkobayashi <suitsitekert@gmail.com>
2025-10-23 21:16:52 +0900

    create

---
+++ src/main/java/com/example/jako_vocab/JakoVocabApplication.java
@@ -0,0 +1,13 @@
+package com.example.jako_vocab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JakoVocabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JakoVocabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jako_vocab/JakoVocabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jako_vocab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JakoVocabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
